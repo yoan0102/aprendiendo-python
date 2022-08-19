31903504974a8d3319602cf21ff3a2e88d0b956d
NPMYoan <yoanestrada01@gmail.com>
2022-08-18 22:14:42 -0400

    variables en python

--- 01-introduccion/main.py
+++ 01-introduccion/main.py
@@ -1,1 +1,16 @@
-print('Hola Mundo')
+# declaracion de una variable
+# Las varibales deben tener nombres descrictivos
+# Las varibales no pueden llamarse con  inguna palabra reservada del lenguaje
+titulo_curso = 'Curso Profesional de Python'
+
+
+# cambio de valor de una variable
+# curso = 'Curso Python'
+
+
+nombre_completo = 'Yoan Estrada Blanco'
+
+
+
+
+print(titulo_curso)
